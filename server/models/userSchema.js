3936adcbf03b81dc8e560643b9f5a16074973627
Kunal-C-S-Shinde <shindekunalchandrakant@gmail.com>
2023-07-14 04:46:49 +0530

    first commit

---
+++ server/models/userSchema.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+
+const userSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+});
+
+const users=new mongoose.model("users",userSchema);
+
+module.exports=users;
